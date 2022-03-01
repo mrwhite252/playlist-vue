b66ba65f451da274af60814b434b0a665b33ee37
DavidBai <baiminghan@gmail.com>
2022-03-01 23:07:51 +0800

    setup the navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/logo-img.svg" />
+      <h1><router-link :to="{ name: 'Home' }">My Playlist</router-link></h1>
+      <div class="links">
+        <button>Log Out</button>
+        <router-link class="btn" :to="{ name: 'Signup' }">Sign Up</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Log In</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navbar {
+  padding: 1em;
+  margin-bottom: 4em;
+  background: white;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 80vw;
+  margin: 0 auto;
+}
+
+nav h1 {
+  margin-left: 1.5em;
+}
+
+nav .links {
+  margin-left: auto;
+}
+
+nav .links a,
+button {
+  margin-left: 1em;
+  font-size: 0.8rem;
+}
+
+nav img {
+  max-height: 4em;
+}
+</style>
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="handleSubmit">
-    <h3>Login</h3>
+    <h3>Log In</h3>
     <input type="email" placeholder="Email" v-model="email" />
     <input type="password" placeholder="Password" v-model="password" />
     <div v-if="error" class="error">{{ error }}</div>
