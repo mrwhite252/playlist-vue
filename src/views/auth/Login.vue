b6230669f98a3407cdfe2a1e99b5ca1358116fee
DavidBai <baiminghan@gmail.com>
2022-03-01 20:56:29 +0800

    add login component and regsitered

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
