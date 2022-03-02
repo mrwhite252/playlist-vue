b2e572a9befa519d016cbadc54ce4e761f3d6684
DavidBai <baiminghan@gmail.com>
2022-03-03 00:03:17 +0800

    setup the queries with firebase

--- src/views/playlist/UserPlaylist.vue
+++ src/views/playlist/UserPlaylist.vue
@@ -1,7 +1,34 @@
-<template>user playlists</template>
+<template>
+  <div class="user-playlists">
+    <h2>My Playlists</h2>
+  </div>
+</template>
 
 <script>
-export default {};
+import getUser from "@/composables/getUser";
+import getCollection from "@/composables/getCollection";
+export default {
+  setup() {
+    const { user } = getUser();
+    const { documents: playlists } = getCollection("playlists", [
+      "userId",
+      "==",
+      user.value.uid,
+    ]);
+
+    console.log(playlists);
+    return { playlists };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  padding-bottom: 10px;
+  margin-bottom: 30px;
+  border-bottom: 1px solid var(--secondary);
+}
+.btn {
+  margin-top: 20px;
+}
+</style>
